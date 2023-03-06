52ce5bf5f1914737ef95fe1daaca8e7c6f5116f2
Philipp Guevorguian <pguevorg@uwo.ca>
2023-03-07 00:20:58 +0400

    implement working merge and indexing

--- utils/fuse_bams.py
+++ utils/fuse_bams.py
@@ -1,3 +1,5 @@
+from tqdm import tqdm
+import pysam
 import os
 import glob
 import argparse
@@ -6,11 +8,23 @@ parser = argparse.ArgumentParser("Generate a list of all the files in a director
 
 parser.add_argument("-d", "--directory", type=str, required=True, help="The directory to search")
 
+parser.add_argument("-o", "--output", type=str, required=True, help="The directory to output to")
+
 args = parser.parse_args()
 dir_of_folders = args.directory
+output_dir = args.output
 
 items = os.listdir(dir_of_folders)
 folders = [item for item in items if os.path.isdir(os.path.join(dir_of_folders, item))]
 
-for folder in folders:
-    print(os.path.join(dir_of_folders, folder))
+for folder in tqdm(folders):
+    folder_path = os.path.join(dir_of_folders, folder)
+    tel_bam = glob.glob(folder_path + '/**/tel.align.bam', recursive=True)[0]
+    base_bam = glob.glob(folder_path + '/**/base.align.bam',recursive = True)[0]
+    print(folder)
+    folder_name = str(folder)
+    bam_name = folder_name + ".bam"
+    merged_bam_file = os.path.join(output_dir,bam_name)
+    os.system(f"samtools merge {merged_bam_file} {base_bam} {tel_bam}")
+    pysam.index(merged_bam_file)
+    
